99c148faaaf99b477f439136b821f456a1511bd7
Endre Jordal <endre.jordal@gmail.com>
2024-01-04 08:49:21 +0100

    Second commit, restructured

---
+++ Previous lectures/Lecture 1.js
@@ -0,0 +1,70 @@
+//console.log("Hello World!") 
+
+// Dette er en kommentar
+
+
+/* fdsjk
+fjklsd
+jksdf
+sdfjk */
+
+//camel case
+let telephoneNumber = 94356195
+telephoneNumber = 94319541
+let mySecretMessage = "Hello world!"
+
+let myVar;
+myVar = 74632
+console.log("Jeg har gått " + (8 * 8) + "km i dag")
+
+
+//console.log(telephoneNumber)
+
+// Data typer
+
+// tekst - string
+const myText = "Dette er tekst."
+
+// tall - integers/int/float
+//const myNumber = 43584673
+
+// boolean
+const myBoolean = false
+let darkModeToggle = true
+
+// array
+const myArray = ["Elliot", "Samuel", "Christian", "Georg"]
+//console.log(myArray[2])
+
+// object
+const myObject = {
+    userName: "Killer961", 
+    userAge: 13, 
+    userEmail: "killer961@gmail.com",
+    isAdmin: false
+}
+//console.log(myObject.userName)
+
+// Operators +, -, *, /
+
+const myNumber = 3 * 10
+const myNumber2 = 3 + 4
+const myNumber3 = myNumber - myNumber2
+
+console.log(myNumber3 + 7)
+
+let lives = 5
+//console.log(lives)
+//lives = lives + 1
+// Shorthand operators:
+//lives += 1
+lives++
+//console.log(lives)
+
+// Comparison operators, <, >, <=, >=, ===, !==
+
+const x = 4
+const y = 4
+
+console.log(x !== y)
+
